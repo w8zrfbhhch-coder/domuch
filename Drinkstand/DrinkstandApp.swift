bf60a738bb412e178bf9b98019c1010bfd167269
Pim <pim@vismans.com>
2026-08-17 12:26:52 +0200

    Initial Commit

---
+++ Drinkstand/DrinkstandApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkstandApp.swift
+//  Drinkstand
+//
+//  Created by Pim on 17/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct DrinkstandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrinkstandTests/DrinkstandTests.swift
@@ -0,0 +1,19 @@
+//
+//  DrinkstandTests.swift
+//  DrinkstandTests
+//
+//  Created by Pim on 17/08/2026.
+//
+
+import Testing
+@testable import Drinkstand
+
+struct DrinkstandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
